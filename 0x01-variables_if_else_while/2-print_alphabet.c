6fdee565fdc3a3dbc780e990acdf3f8f14903cf5
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-11 04:42:07 -0800

    added new file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,22 +8,12 @@
  */
 
 int main(void)
-
 {
-
 	char ch;
-
-
-	for  (ch = 'a'; ch <= 'z'; ch++)
-
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		putchar(ch);
-	
+		putchar(ch);	
 	}
-
-	putchar('\n')
-
-	
-        return (0);
-
+	putchar('\n');
+	return (0);
 }
