8ad26797872ffc5ab049497c474bccd2618fdbca
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-10 17:22:53 -0800

    updated file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,15 +10,15 @@
 int main(void)
 
 {
-printf("Size of a char: %ld byte(s)", sizeof(char));
+printf("Size of a char: %d byte(s)", sizeof(char));
 
-printf("Size of an int: %li byte(s)", sizeof(int));
+printf("Size of an int: %d byte(s)", sizeof(int));
 
-printf("Size of a long int: %lu bytes(s)", sizeof(long int));
+printf("Size of a long int: %d bytes(s)", sizeof(long int));
 
-printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+printf("Size of a long long int: %d byte(s)", sizeof(long long int));
 
-printf("Size of a float: %ld byte(s)", sizeof(float));
+printf("Size of a float: %d byte(s)", sizeof(float));
 
 return (0);
 }
