eb1037f7c447e8915d77461e381e6804827614f4
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-10 16:19:30 -0800

    added 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+
+{
+
+printf("Size of a char: %ld byte(s)", sizeof(char);
+printf("Size of an int: %li byte(s)", sizeof(int));
+printf("Size of a long int: %lu byte(s)", sizeof(long int));
+printf("Size of a long long int: %lu bytes(s)", sizeof(long long int));
+printf("Size of a flost: %ld byte(s)", sizeof(float));
+return (0);
+}
