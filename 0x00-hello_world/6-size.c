50f9f8761edd8876b8060f6753e528be6c90039b
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-10 16:30:47 -0800

    added 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,17 @@ int main(void)
 
 {
 
-printf("Size of a char: %ld byte(s)", sizeof(char);
+printf("Size of a char: %ld byte(s)", sizeof(char));
+
 printf("Size of an int: %li byte(s)", sizeof(int));
-printf("Size of a long int: %lu byte(s)", sizeof(long int));
-printf("Size of a long long int: %lu bytes(s)", sizeof(long long int));
-printf("Size of a flost: %ld byte(s)", sizeof(float));
+
+printf("Size of a long int: %lu bytes(s)", sizeof(long int));
+
+printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+
+printf("Size of a float: %ld byte(s)", sizeof(float));
+
 return (0);
+
+
 }
