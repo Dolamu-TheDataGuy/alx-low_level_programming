5c93f8763421cbccc476914d5f642ffc544806e6
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-15 04:42:08 -0700

    added files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet from a-z 10 times.
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	while (i <= 10)
+	{
+		char j;
+
+		j = 'a';
+
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+	_putchar('\n');
+
+	return (0);
+	}
+	i++;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This a project that treats functions in C
