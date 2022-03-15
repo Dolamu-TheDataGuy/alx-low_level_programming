cb71db7cde8035de42d53dc68cec96ed3bba7573
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-15 03:58:02 -0700

    added 1-alphabet_print

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - prints alphabets from a to z in lower case.
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	if (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	return (0);
+}
+
+
