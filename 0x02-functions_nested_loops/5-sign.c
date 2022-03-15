be6829b122cc325a0e56fe587a46a27b3666449d
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-15 14:43:50 -0700

    added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * print_sign - check for the sign of a number
+ * @n: number to be checked
+ * Return; 1 if n is greater than zero, 0 if n is zero, -1 if n is less than 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);i
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);		
+	}
+}
