e36457534eed6ab517838c29e554af0731cbf600
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-15 06:00:46 -0700

    added file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,4 @@
 #include "main.h"
-
-
 /**
  * print_alphabet - prints alphabets from a to z in lower case.
  * Return: Always 0.
@@ -17,7 +15,7 @@ void print_alphabet(void)
 		_putchar(ch);
 		ch++;
 	}
-	return (0);
+	return 0;
 }
 
 
