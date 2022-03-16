e634756daeeeec5c2d79b0af76fc7eaffbfe7513
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-16 04:10:23 -0700

    added print_to_98 code

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print any number inputed to 98
+ * @n: integer that is to be printed first
+ * Return: integers from n to 98
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		int i;
+
+		for (i = n; n >= 98; n--)
+		{
+			_putchar("%d", n);
+			if (n != 98)
+			{
+				_putchar(",");
+				_putchar(" ");
+			}
+		}
+	}
+	else if (n < 98)
+	{
+		int i;
+
+		for (i = n; n <= 98; n++)
+		{
+			_putchar("%d", n)
+			if (n != 98)
+			{
+				_putchar(",");
+				_putchar(" ");
+			}
+		}
+	}
+	else
+	{
+		_putchar("%d", n);
+	}
+}
