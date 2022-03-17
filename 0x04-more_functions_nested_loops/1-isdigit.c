6a4aab8a1fd09389acca3d9f22d154c51c656b16
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-17 10:09:11 -0700

    pushed isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 
 /**
  * _isdigit - checks if a number is a digit between 0 and 9
- * c: number to be checked
+ * @c: number to be checked
  * Return: 1 if c is a digit else 0
  */
 
 int _isdigit(int c)
 {
-	return (c >= '0' && c <= '9')
+	return (c >= '0' && c <= '9');
 }
 
 
