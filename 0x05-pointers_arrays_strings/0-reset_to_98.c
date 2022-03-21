d5b35d58bf49ba5d2aa0807098155c67e7b5967f
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-21 14:39:44 -0700

    added first file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/** reset_to_98: a function that collects an integer pointer and 
+ * return or update to 98
+ * @n: number to be update
+ * Return: Return nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n =98;
+}
