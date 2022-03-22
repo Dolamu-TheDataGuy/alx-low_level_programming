f86cc76270b3bbcbc0a40f421f7d72e0bde97b84
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-21 19:30:25 -0700

    added 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main"
+
+/**
+ * puts2 - a function that prints every other character of a string
+ * starts with first charactr and is followed by new line
+ * @str : string to be printed
+ *
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int c = 0;
+
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+
+	for (i = 0; i < 0; i++)
+	{
+		if (i % 2 != 0)
+			continue;
+		_putchar(str[i]);
+	}
+	_putchar(10);
+}
