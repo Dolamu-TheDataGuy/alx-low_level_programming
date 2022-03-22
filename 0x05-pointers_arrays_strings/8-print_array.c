99e4ad6765330f06f5695f81cf7c9b061c3df48d
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-21 20:48:37 -0700

    add changes

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,12 +14,13 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
+		printf("%d", *(a + 1));
+
 		if (i != n - 1)
 		{
 			printf(", ");
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
 
