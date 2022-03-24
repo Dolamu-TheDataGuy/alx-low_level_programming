e4b253c9d47171228f110f7a882eba370801cbab
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-24 16:16:15 -0700

    added sedond file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings
+ * @dest: string
+ * @src: string
+ * @n: number of elements to concatenate
+ * Return: pointer to resulting 'dest'
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	for (a = 0; dest[a] != 0; a++)
+		;
+	for (b = 0; src[b] != '\0' && n > 0; b++, n--, a++)
+	{
+		dest[a] = src[b];
+	}
+	return (dest);
+}
