db768dcd90bee1da1de35917b94fae30aa40efa7
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-24 14:43:41 -0700

    added first file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ *
+ * @dest: character of destination variable
+ * @src: character of source variable
+ *
+ * Return: a pointer to the resulting string destination
+ */
+
+char *_strcat(char *dest, char *src)
+{i
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+
+	dest[i + j] = '\0';
+
+	return (dest);
+}
