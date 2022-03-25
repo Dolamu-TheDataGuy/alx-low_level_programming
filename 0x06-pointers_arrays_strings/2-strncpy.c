da0a9c5564c39a6a7a4aa915977a04f8b4eaeb64
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-24 19:25:25 -0700

    added third file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include"main.h"
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: destination character
+ * @src: source character variable
+ * @n: int type variable
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		if (i < n)
+		{
+			dest[i] = src[i];
+		}
+	}
+
+	j = i;
+
+	for (; n > j; j++)
+	{
+		dest[j] = '\0';
+	}
+
+	return (dest);
+}
