df4374571a6ae09167ad07e430e38563c2945120
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-28 16:45:00 -0700

    added file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,12 +10,16 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
-	i = 0;
-	for (i = 0; i <= n; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
-}
+	char *yolo;
 
+	yolo = dest;
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	return (yolo);
+}
