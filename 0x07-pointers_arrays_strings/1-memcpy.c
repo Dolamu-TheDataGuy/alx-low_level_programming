b50f17240ffb0c0e90674e1abdf1f7f7720fb380
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-28 14:13:53 -0700

    added memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy data from a source address to a destination address
+ * @dest: character array to copy into
+ * @src: character array to copy from
+ * @n: number of elements to copy.
+ * Return: pointer to 'dest'
+ */
+
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; i <= n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
+
