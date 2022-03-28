e265c7c566a21cb9a5fcac992361d7385076998c
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-03-28 15:54:10 -0700

    added file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,18 +8,19 @@
  * Return: pointer to 'dest'
  */
 
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	char *yolo;
 
-	i = 0;
-
-	for (i = 0; i <= n; i++)
+	yolo = dest;
+	while (n > 0)
 	{
-		dest[i] = src[i];
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
 	}
 
-	return (dest);
+	return (yolo);
 }
 
