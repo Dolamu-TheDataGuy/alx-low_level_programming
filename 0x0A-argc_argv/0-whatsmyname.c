30070e396330a37049dd1d623f7df22826cc1268
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-04-04 01:35:39 -0700

    added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include main.h
+#include <stdio.h>
+
+
+/**
+ * main - a program that prints its name followed by a new line.
+ * @argc: argument count for command line.
+ * @argv: argument value, a string that comes after a function.
+ * Return: always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This task covers concepts on command line arguments in C
