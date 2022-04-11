ce65c7fc4407de6ac3bf758bf52ff43498348aac
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-04-11 13:02:26 -0700

    added file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the name of a file
+ *
+ * Returns: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
