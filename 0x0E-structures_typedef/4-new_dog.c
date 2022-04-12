8266dbbc748397913bcaf53943d4d2c98f9a5271
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-04-11 19:31:32 -0700

    added files

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,14 +20,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	len_name = strlen(name);
+	len_name = _strlen(name);
 	new_dog->name = malloc(sizeof(char) * len_name + 1);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->namee = _strcpy(new_dog->name, name);
+	new_dog->name = _strcpy(new_dog->name, name);
 	len_owner = _strlen(owner);
 	new_dog->owner = malloc(sizeof(char) * len_owner + 1);
 	if (new_dog->owner == NULL)
