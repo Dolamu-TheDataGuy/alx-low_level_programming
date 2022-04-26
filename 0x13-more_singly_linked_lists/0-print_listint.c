2c95f4f859d33f2eee45432c0c6496ed1b7ca482
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-04-26 14:12:08 -0700

    added printelement

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,14 +10,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t nbr_nodes = 0;
 	const listint_t *current = h;
 
 	while (current != NULL)
 	{
 		printf("%i\n", current->n);
 		current = current->next;
-		num++;
+		nbr_nodes++;
 	}
-	return (num);
+	return (nbr_nodes);
 }
