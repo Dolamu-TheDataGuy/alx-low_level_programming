9c2f2ef4db2f891d57e2f1f2ff139444e130c277
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-04-26 15:56:06 -0700

    added node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,14 +8,23 @@
  * Return: number of elements
  */
 
-listint_t *add_nodeint(listint_t *head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node = (listint_t *) malloc(sizeof(listint_t));
+	listint_t *new_node;
+
+	new_node = (listint_t *) malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
 	new_node->n = n;
-	new_node->next = NULL;
 
-	new_node->next = head;
-	head = new_node;
-	return (head);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
+
