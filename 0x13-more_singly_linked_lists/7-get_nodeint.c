f1d0424ab907f7e76be748a017b9ba3b4ebe4e67
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-04-26 17:29:07 -0700

    added getindex

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,13 +3,13 @@
 /**
  * get_nodeint_at_index - returns the nth node of a listint_t linked list.
  * @head:head of linked list
- * @index: node inde to return
- * Retun: node
+ * @index: node index to return
+ * Return: node
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint *current;
+	listint_t *current;
 
 	if (head == NULL)
 		return (0);
