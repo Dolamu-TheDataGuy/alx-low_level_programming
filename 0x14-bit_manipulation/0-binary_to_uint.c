426b8e0176b2476677eac9fcf1edaba7f5eaf183
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-04-28 17:40:13 -0700

    added binary to integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_to_unit - converts a binary number to an unsigned integer
+ * @b: The binary string to convert
+ *
+ * Return: The positive number converted from a binary
+ */
+
+unsigned int binary_to_unit(const char *b)
+{
+	unsigned int len = 0, count = 0, sum = 0;
+
+	if (b == NULL)
+		return (0);
+
+	len = _strlen(b);
+
+	while (len--)
+	{
+		if (b[len] != 48 && b[len] != 49)
+			return (0);
+
+		if (b[len] == 49)
+			sum += 1 << count;
+
+		else if (b[len] == 48)
+			sum += 0;
+
+		count++;
+	}
+
+	return (sum);
+}
+
+/**
+ * _strlen - function to determine the length of a string
+ * @s: string to count
+ *
+ * Return: string length
+ */
+
+int _strlen(const char *s)
+{
+	int num = 0;
+
+	while (s[num])
+	{
+		num++;
+	}
+	return (num);
+}
+
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This is a repo about task on bitwise operators and bit manipulation
