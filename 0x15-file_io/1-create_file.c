5fa96b909d278eb1e33cc16e50b49b0b0bce1681
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-05-02 20:41:57 -0700

    added createfile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * create_file - function that creates a file.
- * @filename: file to be created
- * @text_content: NULL terminated string
- *
- * Return: 1 on Success, -1 on Failure
- */
-
+  * create_file - ...
+  * @filename: ...
+  * @text_content: ...
+  *
+  * Return: ...
+  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -19,27 +18,25 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_context)
-		write(fd, text_context, _strlen(text_context));
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
 
 	close(fd);
 	return (1);
 }
 
 /**
- * _strlen - Returns the length of a string
- * @s: String to be counted
- *
- * Return: String length
- */
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
 int _strlen(char *s)
 {
 	int c = 0;
 
 	while (s[c])
-	{
 		c++;
-	}
 
 	return (c);
 }
