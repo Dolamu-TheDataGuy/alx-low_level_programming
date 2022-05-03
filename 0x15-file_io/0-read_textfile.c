c6177bcc3087b0e02da0d626aeec1564dbe9b1f2
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-05-02 19:57:34 -0700

    added read text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * read_textfile - function that reads a text file and prints it
- * @filename: The source file
- * @letters: Number of letters to read and print
- *
- * Return: number of letter it could print
- */
-
+  * read_textfile - ...
+  * @filename: The source file
+  * @letters: Number of letters to reads and prints
+  *
+  * Return: ...
+  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, readed;
@@ -21,10 +20,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
-		return(0);
+		return (0);
 
 	readed = read(fd, buff, letters);
-	write(STDOUT_FILNO, buff, readed);
+	write(STDOUT_FILENO, buff, readed);
 
 	free(buff);
 	close(fd);
