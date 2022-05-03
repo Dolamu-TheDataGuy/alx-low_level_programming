e1442b6234c7e4108bf43fe6d397a27f97553707
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-05-02 20:36:46 -0700

    added createfiles

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: file to be created
+ * @text_content: NULL terminated string
+ *
+ * Return: 1 on Success, -1 on Failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_context)
+		write(fd, text_context, _strlen(text_context));
+
+	close(fd);
+	return (1);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to be counted
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+	{
+		c++;
+	}
+
+	return (c);
+}
