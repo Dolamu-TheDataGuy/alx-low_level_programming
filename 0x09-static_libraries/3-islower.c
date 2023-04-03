d6f6d1d0fa5033e9399598fad4194c053b8d7f6f
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-04-03 20:07:15 +0100

    added 3-islower function

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,7 +1,19 @@
 #include "main.h"
 
-int _islower(int c __attribute__((unused)))
+/**
+ * _islower - tests whether a character is a
+ * lowercase letter from the English alphabet.
+ * @c: character to test.
+ * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
+ */
+int _islower(int c)
 {
-	return (0);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
-
