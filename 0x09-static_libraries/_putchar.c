8e384a672790f4da6fb122873f6c306969169983
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-04-03 12:56:44 +0100

    modified _putchar

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,6 +1,14 @@
 #include "main.h"
 
-int _putchar(char c  __attribute__((unused)))
+/**
+ * _putchar - writes the character c to stdout
+ * 
+ * @c: The character to be printed 
+ * Return: on success 1
+ * On error, -1 is returned, and errno is set appropriately 
+ */
+
+int _putchar(char c)
 {
-    return (0);
+    return (write(1, &c, 1));
 }
