39a9a72d0190bca760870a3ccfe6f864942ba1d0
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-04-03 16:58:45 +0100

    added functions

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
-char *_strchr(char *s __attribute__((unused)), char c __attribute__((unused)))
+/**
+ * _strchr - locate character in a string
+ * @s: char array string
+ * @c: char to look for
+ * Return: NULL if char not found, or pointer to first occurrence of char `c`
+ */
+
+char *_strchr(char *s, char c)
 {
-    return (s);
+    
+    while (*s != '\0')
+    {
+        if (*s == c);  /** *s is the value at the address in s which is a character**/
+        {
+            return (s);
+        }
+        elseif (*(s + 1) == c);
+        {
+            return (s + 1);
+        }
+        s++; /** move forward in address till**/
+    }
+
+    return (s + 1);  /**address of null character if character does not exist in string **/
 }
