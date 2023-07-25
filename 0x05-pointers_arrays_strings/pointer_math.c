9c74de13cb5282d5c4bb14f7bdf088155f3a3060
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-07-25 17:51:13 +0100

    pointer arithmetic in structs comment

--- 0x05-pointers_arrays_strings/pointer_math.c
+++ 0x05-pointers_arrays_strings/pointer_math.c
@@ -10,7 +10,7 @@ typedef struct packet_t {
 int main (int argc, char **argv) {
     packet_t *packets = malloc(sizeof(packet_t)* 10);
 
-    packet_t *p2 = (packets + 2);
+    packet_t *p2 = (packets + 2); // packets + (2 *(sizeof(packet_t)) bytes
 
     printf("packets = %p, p2 = %p. sizeof(packet_t) = %lu\n", packets, p2, sizeof(packet_t));
 }
