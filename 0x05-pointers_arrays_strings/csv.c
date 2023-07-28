962e05e07056fc7c8b3ca10ce0034d78db0ccd62
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-07-28 12:45:23 +0100

    used regex/array/string and character format to capture delimiter

--- 0x05-pointers_arrays_strings/csv.c
+++ 0x05-pointers_arrays_strings/csv.c
@@ -7,9 +7,18 @@
 int main(int argc, char **argv) {
 
     char item[BUFSIZE];
+    char delim;
+    int  item_count = 0;
 
-    while (scanf("%s", item) != EOF) {
-        printf("item: %s\n", item);
+    // [^,\n] reads all the character as a string and put into the item array
+    // until it encounters the "," or "/n" character, then the delimiter is stored in the address of the delim variable.
+    while (scanf("%[^,\n]%c", item, &delim) != EOF) {
+        printf("%s, ", item);
+        item_count++;
+        if (delim == '\n') {
+            printf("(%d items)\n", item_count);
+            item_count = 0;
+        }
     }
 }
  
